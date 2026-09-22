8ccad0123f1edeb55a8725a399ea1be94eac18e6
DiaxJF <trabajosjuan0125@gmail.com>
2026-09-22 02:31:55 +0000

    .

---
+++ src/act.js
@@ -0,0 +1,7 @@
+let productos = [ 
+{ id : 21234, nombre : "jamon", valor :100, categoria:"alimentos" },
+{ id : 123456, nombre : "tv", valor :100000, categoria : "electrodomesticos" },
+{ id : 654321, nombre : "pera", valor :2000, categoria : "alimentos" },
+{ id : 987321, nombre : "maxsteel", valor :10000, categoria : "juguetes" },
+{ id : 123789, nombre : "camisa", valor :10000, categoria : "ropa" },
+] ;
